e9017b3faef33d23c2942103e242a1e8b835be54
Valerio Lyndon <valeriolyndon@gmail.com>
2020-03-05 21:42:27 -0800

    Add error tracking.
    - DB now tracks how many times an entry has thrown an error and removes it if it hits too many.
    - Moved delay before requests to prevent cancelled actions affecting the delays.
    - Improve logging slightly.
    - Fixed old 404 values not being overridden with fresh data (lines 248/253).

--- database/construct_database.py
+++ database/construct_database.py
@@ -16,6 +16,9 @@ import requests
 import urllib.request
 from bs4 import BeautifulSoup
 
+#Misc
+import traceback
+
 # VARIABLES & SETUP
 
 debug = False
@@ -40,7 +43,8 @@ c.execute('''
 		id INT,
 		name TEXT,
 		image TEXT,
-		updated TEXT
+		updated TEXT,
+		error TEXT
 	)
 ''')
 
@@ -53,6 +57,13 @@ def encodeString(s):
 
 def decodeString(s):
 	return urllib.parse.unquote(s)
+
+# Formats string-formatted dates into datetime objects
+
+def formatDate(d):
+	return datetime.strptime(d, timeFormat)
+
+# Logs string to console and file simultaneously.
 
 def log(msg):
 	output = f'{strftime("%H:%M:%S")} {msg}'
@@ -71,12 +82,10 @@ if debug: log('DEBUG ENABLED')
 # Estimate total MAL database entries
 
 def estimateTotalEntries(listType):
+	print(f'Sending web request - waiting {delay} seconds to avoid spam protection...')
+	sleep(delay)
 	url = 'https://myanimelist.net/' + listType + '.php?o=9&c[0]=a&c[1]=d&cv=2'
 	parsed = BeautifulSoup(requests.get(url).text, 'html.parser')
-	
-	# Add delay - uses print instead of log since time delays will only matter when viewing live
-	print(f'Sent web request - waiting {delay} seconds to avoid spam protection...')
-	sleep(delay)
 	
 	recent = parsed.find('a', class_='hoverinfo_trigger')
 	recentId = recent['href'].split('/')[4]
@@ -111,17 +120,15 @@ def parseEntry(listType, id):
 		'id': id,
 		'name': '_null_',
 		'image': '_null_',
-		'updated': str(datetime.utcnow())
+		'updated': str(datetime.utcnow()),
+		'error': ['None']
 	}
 	
 	# Fetch result
 	
+	sleep(delay)
 	url = 'https://myanimelist.net/' + listType + '/' + str(id)
 	parsed = BeautifulSoup(requests.get(url).text, 'html.parser')
-	
-	# Add delay - uses print instead of log since time delays will only matter when viewing live
-	#print(f'Sent web request - waiting {delay} seconds to avoid spam protection...')
-	sleep(delay)
 	
 	# Check for misc problems (504 errors, page load faults, etc)
 	
@@ -130,7 +137,7 @@ def parseEntry(listType, id):
 	if malHealth is None:
 		data['name'] = '_error_'
 		data['image'] = '_error_'
-		data['error'] = 'unknown'
+		data['error'] = ['unknown', 1]
 		return data
 	
 	# Check for 404
@@ -140,7 +147,7 @@ def parseEntry(listType, id):
 	if deadEntry is not None:
 		data['name'] = '_404_'
 		data['image'] = '_404_'
-		data['error'] = '404'
+		data['error'] = ['404', 1]
 		return data
 		
 	# Fetch new data
@@ -169,7 +176,7 @@ def updateById(listType, id):
 	# Check for existence in DB
 	
 	c.execute(f'''
-		SELECT type, id, name, image, updated
+		SELECT type, id, name, image, updated, error
 		FROM data
 		WHERE type="{listType}"
 		AND id={id}
@@ -181,15 +188,15 @@ def updateById(listType, id):
 		
 		sqlCommand = '''
 			INSERT INTO data
-			(type, id, name, image, updated)
-			VALUES("{type}", {id}, "{name}", "{image}", "{updated}")
+			(type, id, name, image, updated, error)
+			VALUES("{type}", {id}, "{name}", "{image}", "{updated}", "{error}")
 		'''
 		
 		# Log Changes
-		if newData.get('error') == '404':
+		if newData['error'][0] == '404':
 			logSuffix = 'Added new (404)'
-		elif newData.get('error') != None:
-			logSuffix = 'Added new (unknown error)'
+		elif newData['error'][0] != 'None':
+			logSuffix = f'Added new ({newData["error"][0]} error)'
 		else:
 			logSuffix = 'Added new'
 	
@@ -200,7 +207,8 @@ def updateById(listType, id):
 			UPDATE data
 			SET name="{name}",
 				image="{image}",
-				updated="{updated}"
+				updated="{updated}",
+				error="{error}"
 			WHERE type="{type}"
 			AND id={id}
 		'''
@@ -210,35 +218,44 @@ def updateById(listType, id):
 			'id': entry[1],
 			'name': entry[2],
 			'image': entry[3],
-			'updated': entry[4]
+			'updated': entry[4],
+			'error': entry[5].split(', ')
 		}
 		
 		# Handle data
 		
-		if 'error' in newData:
-			# Retain old data if error - this may change in future but for now it prioritizes maintaining data in case it's a 404 caused by other issues
-			newData['name'] = oldData['name']
-			newData['image'] = oldData['image']
+		if newData['error'][0] != 'None':
+			# Check for previous errors and add their counts together
+			daysSinceLast = abs(formatDate(oldData['updated']) - formatDate(newData['updated'])).days
 			
-			if newData['error'] == '404':
-				logSuffix = 'nothing updated (404)'
+			if daysSinceLast > 3 and newData['error'][0] == oldData['error'][0]:
+				newData['error'][1] = int(newData['error'][1]) + int(oldData['error'][1])
+			
+			# Sets data as 404 if entry has 404'd 5 times
+			# if below threshold or generic error it retains old data
+			if newData['error'][0] == '404' and int(newData['error'][1]) > 4:
+				logSuffix = 'entry set as 404 - reached max 404s'
+			else:
+				newData['name'] = oldData['name']
+				newData['image'] = oldData['image']
 				
-			elif newData['error'] is not None:
-				# Generic error handling
-				logSuffix = f'error: {newData["error"]}'
+				if newData['error'][0] == '404':
+					logSuffix = 'nothing updated (404)'
+				
+				else:
+					# Generic error handling
+					logSuffix = f'error: {newData["error"][0]}'
 		
 		else:
 			updated = []
 			
-			if newData['name'] == '_null_':
+			if newData['name'] == '_null_' and oldData['name'] != '_404_' and oldData['name'] != '_error_':
 				newData['name'] = oldData['name']
-			
 			elif newData['name'] != oldData['name']:
 				updated += ['name']
 			
-			if newData['image'] == '_null_':
+			if newData['image'] == '_null_' and oldData['image'] != '_404_' and oldData['image'] != '_error_':
 				newData['image'] = oldData['image']
-			
 			elif newData['image'] != oldData['image']:
 				updated += ['image']
 			
@@ -252,14 +269,17 @@ def updateById(listType, id):
 	
 	# Commit Changes
 	
-	sqlCommand = sqlCommand.format(type=newData['type'], id=newData['id'], name=newData['name'], image=newData['image'], updated=newData['updated'])
+	if debug: print(newData)
+	
+	formattedError = ', '.join(str(x) for x in newData['error'])
+	
+	sqlCommand = sqlCommand.format(type=newData['type'], id=newData['id'], name=newData['name'], image=newData['image'], updated=newData['updated'], error=formattedError)
 	
 	if debug: print(sqlCommand)
 	c.execute(sqlCommand)
 	if not debug: conn.commit()
 	
 	log(f'{logPrefix} {logSuffix}')
-	
 	return newData
 
 # Build Database
@@ -313,15 +333,14 @@ def build(listType):
 			data = updateById(listType, id)
 			
 			# Count errors
-			if data.get('error') == '404':
+			if data['error'][0] == '404':
 				errorStreak += 1
-			
 			else:
 				errorStreak = 0
 		
 		log('FINISHED BUILDING')
-	except Exception as e:
-		log('ERROR: ' + str(e))
+	except:
+		log('ERROR: ' + traceback.format_exc())
 
 # Maintain Database
 
@@ -336,7 +355,7 @@ def maintain():
 	# Select entries to update
 	
 	c.execute(f'''
-		SELECT type, id, name, image, updated
+		SELECT type, id, name, image, updated, error
 		FROM data
 		ORDER BY updated ASC
 	''')
@@ -355,7 +374,8 @@ def maintain():
 			'id': entry[1],
 			'name': entry[2],
 			'image': entry[3],
-			'updated': entry[4]
+			'updated': entry[4],
+			'error': entry[5].split(', ')
 		}
 		
 		if currentData['type'] == 'anime':
@@ -387,9 +407,9 @@ def maintain():
 		# Combine formulas to find total weight
 		entry.append(weightTime * weightId)
 	
-	entries.sort(key = lambda col: col[5])
+	entries.sort(key = lambda col: col[6])
 	
-	# Begin maintaining
+	# Begin updating
 	
 	try:
 		for entry in entries:
@@ -398,12 +418,13 @@ def maintain():
 				'id': entry[1],
 				'name': entry[2],
 				'image': entry[3],
-				'updated': entry[4]
+				'updated': entry[4],
+				'error': entry[5].split(', ')
 			}
 			
-			# Skip blanks
+			# Skip 404s
 			
-			if currentData['name'] == '_404_':
+			if len(currentData['error']) > 1 and int(currentData['error'][1]) > 4:
 				continue
 			
 			# Update entry
@@ -411,8 +432,8 @@ def maintain():
 			updateById(currentData['type'], currentData['id'])
 		
 		log('MAINTENANCE COMPLETE')
-	except Exception as e:
-		log('ERROR: ' + str(e))
+	except:
+		log('ERROR: ' + traceback.format_exc())
 
 # Commands
 
